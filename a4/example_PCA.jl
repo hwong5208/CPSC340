b9693c32330847ca4c4c2bb09b59d99f8b8ef469
hwong5208 <hwong5208@gmail.com>
2017-11-14 21:22:01 -0800

    Changes to be committed: 	modified:   a4/a4.tex 	modified:   a4/example_PCA.jl 	new file:   a4/q3_2_1_a.png 	new file:   a4/q3_2_1_b.png

--- a4/example_PCA.jl
+++ a4/example_PCA.jl
@@ -3,9 +3,50 @@ dataTable = readcsv("animals.csv")
 X = float(real(dataTable[2:end,2:end]))
 (n,d) = size(X)
 
+
+
+function FrobeniusNorm(X)
+     X0 = X.^2
+     X1 = sum(X0)
+     return X1
+end
+
+function VarianceRemain(ZW,X)
+
+    return 1- vecnorm(ZW-X).^2/vecnorm(X).^2
+end
+
 # Standardize columns
 include("misc.jl")
 (X,mu,sigma) = standardizeCols(X)
+
+# # Plot matrix as image
+# using PyPlot
+# figure(1)
+# clf()
+# imshow(X)
+
+# # Show scatterplot of 2 random features
+# j1 = rand(1:d)
+# j2 = rand(1:d)
+# figure(2)
+# clf()
+# plot(X[:,j1],X[:,j2],".")
+# for i in rand(1:n,10)
+#     annotate(dataTable[i+1,1],
+# 	xy=[X[i,j1],X[i,j2]],
+# 	xycoords="data")
+# end
+
+
+include("PCA.jl")
+k =13
+model = PCA(X,k)
+
+Z = model.compress(X)
+ZW=model.expand(Z)
+
+print(VarianceRemain(ZW,X))
 
 # Plot matrix as image
 using PyPlot
@@ -14,13 +55,14 @@ clf()
 imshow(X)
 
 # Show scatterplot of 2 random features
-j1 = rand(1:d)
-j2 = rand(1:d)
+
 figure(2)
 clf()
-plot(X[:,j1],X[:,j2],".")
-for i in rand(1:n,10)
+plot(Z[:,1],Z[:,2],".")
+for i in (1:n)
     annotate(dataTable[i+1,1],
-	xy=[X[i,j1],X[i,j2]],
-	xycoords="data")
+    xy=[Z[i,1],Z[i,2]],
+    xycoords="data")
 end
+
+
