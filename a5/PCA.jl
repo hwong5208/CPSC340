648c00e2db1aac970c83fdec8854e11badab1600
hwong5208 <hwong5208@gmail.com>
2017-11-27 22:12:27 -0800

    Changes to be committed: 	modified:   a5/MDS.jl 	modified:   a5/PCA.jl 	modified:   a5/a5.tex 	modified:   a5/example_RPCA.jl 	new file:   a5/q2_1.png 	new file:   a5/q2_2.png

--- a5/PCA.jl
+++ a5/PCA.jl
@@ -71,6 +71,7 @@ end
 
 function compress_gradientDescent(Xhat,W,mu)
     (t,d) = size(Xhat)
+    k = size(W,1)
     Xcentered = Xhat - repmat(mu,t,1)
     Z = zeros(t,k)
 
@@ -88,7 +89,7 @@ function pcaObjZ(z,X,W)
 
     # Comptue function value
     R = Z*W - X
-    f = sum(R.^2)
+    f = (1/2)sum(R.^2)
 
     # Comptue derivative with respect to each residual
     dR = R
@@ -108,7 +109,7 @@ function pcaObjW(w,X,Z)
 
     # Comptue function value
     R = Z*W - X
-    f = sum(R.^2)
+    f = (1/2)sum(R.^2)
 
     # Comptue derivative with respect to each residual
     dR = R
@@ -116,6 +117,95 @@ function pcaObjW(w,X,Z)
     # Multiply by Z' to get elements of gradient
     G = Z'dR
 
+    # Return function and gradient vector
+    return (f,G[:])
+end
+
+
+
+function robustPCA(X,k)
+    (n,d) = size(X)
+
+    # Subtract mean
+    mu = mean(X,1)
+    X -= repmat(mu,n,1)
+
+    # Initialize W and Z
+    W = randn(k,d)
+    Z = randn(n,k)
+
+    R = Z*W - X
+    epsilon=0.0001
+    f= sum(sum(sqrt.(R.^2+epsilon)))
+    funObjZ(z) = rpcaObjZ(z,X,W)
+    funObjW(w) = rpcaObjW(w,X,Z)
+    for iter in 1:50
+        fOld = f
+
+        # Update Z
+        Z[:] = findMin(funObjZ,Z[:],verbose=false,maxIter=10)
+
+        # Update W
+        W[:] = findMin(funObjW,W[:],verbose=false,maxIter=10)
+
+        R = Z*W - X
+        f = sum(sum(sqrt.(R.^2+epsilon)))
+        @printf("Iteration %d, loss = %f\n",iter,f/length(X))
+
+        if (fOld - f)/length(X) < 1e-2
+            break
+        end
+    end
+
+
+    # We didn't enforce that W was orthogonal so we need to optimize to find Z
+    compress(Xhat) = compress_gradientDescent(Xhat,W,mu)
+    expand(Z) = expandFunc(Z,W,mu)
+
+    return CompressModel(compress,expand,W)
+end
+
+function rpcaObjZ(z,X,W)
+    # Rezie vector of parameters into matrix
+    n = size(X,1)
+    k = size(W,1)
+    Z = reshape(z,n,k)
+
+    # Comptue function value
+    R = Z*W - X
+        epsilon=0.0001
+    A = (sqrt.(R.^2+epsilon))
+
+    # Comptue derivative with respect to each residual
+    dR = R./A
+
+    # Multiply by W' to get elements of gradient
+    G = dR*W'
+
+     f = sum(sum(sqrt.(R.^2+epsilon)))
+    # Return function and gradient vector
+    return (f,G[:])
+end
+
+function rpcaObjW(w,X,Z)
+    # Rezie vector of parameters into matrix
+    d = size(X,2)
+    k = size(Z,2)
+    W = reshape(w,k,d)
+
+    # Comptue function value
+    R = Z*W - X
+        epsilon=0.0001
+    A = (sqrt.(R.^2+epsilon))
+
+    # Comptue derivative with respect to each residual
+    dR = R ./A
+
+    # Multiply by Z' to get elements of gradient
+    G = Z'dR
+
+
+     f = sum(sum(sqrt.(R.^2+epsilon)))
     # Return function and gradient vector
     return (f,G[:])
 end
